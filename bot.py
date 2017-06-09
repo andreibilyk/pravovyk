5d4b63c12fb7967ade6fbbb7b9da497946f48437
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-06-09 22:07:52 +0300

    init

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ def repeat_all_messages(message):
    url_button = types.InlineKeyboardButton(text="Підключити оператора", url="https://t.me/andrei_bilyk")
    keyboard.add(url_button)
    bot.send_message(message.chat.id,row[1]+"<b>Не знайшли відповідь?</b>",parse_mode='HTML',reply_markup = keyboard)
-   bot.send_sticker(chat_id,'AAQCABNLxOMNAATaPvbKYonrhLQyAAIC')
+   bot.send_photo(chat_id,'AAQCABNLxOMNAATaPvbKYonrhLQyAAIC')
   try:
    file_id = db_worker.select_file(text)
    bot.send_document(message.chat.id,file_id)
