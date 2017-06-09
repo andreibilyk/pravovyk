08a0eda0cf59373de7e6391f57f8efb44faf0dd8
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-06-09 22:43:40 +0300

    init

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ def repeat_all_messages(message):
    keyboard = types.InlineKeyboardMarkup()
    url_button = types.InlineKeyboardButton(text="Перейти на веб-сайт", url="http://pravovyk.com")
    keyboard.add(url_button)
-   bot.send_message(message.chat.id,"",reply_markup = keyboard)
+   bot.send_message(message.chat.id,"Дізнайтесь більше про нас😎",reply_markup = keyboard)
   try:
    file_id = db_worker.select_file(text)
    bot.send_document(message.chat.id,file_id)
