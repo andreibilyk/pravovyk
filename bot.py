040a3f8db5713eb155f1d0a4997749b2aaac6305
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-06-12 21:04:23 +0300

    init

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ def sms_verification(message):
   validate_mobile(message.text)
   bot.send_message(message.chat.id,"Success")
  except BaseException as e:
-  bot.send_message(message.chat.id,str(e))
+  bot.send_message(message.chat.id,"Номер телефону введений некоректно. Спробуйте ще раз")
 
 
 def validate_mobile(value):
