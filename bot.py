1b7365560a09e4996760cdc5aef042c53e90ac62
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-06-13 11:00:48 +0300

    init

--- bot.py
+++ bot.py
@@ -152,7 +152,7 @@ def code_verif(message):
   markup.add("Ми в соц.мережах🤓🤳")
   markup.add("Поділитися з друзями👥")
   user.verified = True
-  msg = bot.send_message(message.chat.id,"Верифікація пройшла успішно😊Давай почнемо нашу бесіду?😃 Обери сферу:",reply_markup = markup)
+  msg = bot.send_message(message.chat.id,"Верифікація пройшла успішно😊Давай почнемо нашу бесіду!😃 Обери сферу:",reply_markup = markup)
   bot.register_next_step_handler(msg,main_messages)
  else:
   keyboard = types.InlineKeyboardMarkup()
