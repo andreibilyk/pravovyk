91821c974f4a79f052be4ae7a0e10d1b1b6bff3c
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-06-17 12:43:16 +0300

    init

--- SQLighter.py
+++ SQLighter.py
@@ -20,7 +20,7 @@ class SQLighter:
             #print ('SELECT * FROM user_interac WHERE user_answer = %s'% "'"+answer.decode('utf-8')+"'")
             #self.cursor.execute('SELECT * FROM user_interac WHERE user_answer = %s '% "'"+ str(answer.decode('utf-8'))+"'")
             self.cursor.execute('''SELECT * FROM user_interac WHERE user_answer = 'Сімейне право👨‍👩‍👧‍👦' ''')
-            print(self.cursor.fetchall())
+            print(self.cursor.fetchall()[0])
             return self.cursor.fetchall()[0]
 
     def select_file(self,answer):
