f123a0dbe3064c3577df7a134808ace5c32fa274
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-09 14:14:15 +0300

    init

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ def main_messages(message):
             u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
             u"\u200d"
             u"\u23f3"#⏳
-            u"\u2642"#
+            u"\u2642"#♂
                                "]+", flags=re.UNICODE)
     gog_text = emoji_pattern.sub(r'', text)
     print(gog_text)
