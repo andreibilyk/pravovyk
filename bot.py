8202cb25d29df3e433f72ac3dbc3710205820267
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-10 09:00:02 +0300

    init

--- bot.py
+++ bot.py
@@ -35,10 +35,6 @@ def handle_commands(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def main_messages(message):
- if user.verified == False:
-  if(db_worker.seekChatid(message.chat.id)):
-    user.verified = True
- if user.verified == True:
   if not hasattr(main_messages, '_steps'):  # инициализация значения
    main_messages._steps = []
   text = message.text
