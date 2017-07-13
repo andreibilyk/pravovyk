23489b99de293c603e86df49964cae1c375f8a85
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-13 14:17:49 +0300

    init

--- utils.py
+++ utils.py
@@ -34,15 +34,32 @@ def generate_markup(answers,callback):
     print(callback)
     markup = types.InlineKeyboardMarkup()
     # Склеиваем правильный ответ с неправильными
+        emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\u200d"
+                u"\u23f3"#⏳
+                u"\u2642"#♂
+                u"\ufe0f"
+                u"\u2699"#⚙️
+                u"\U0001f913"
+                u"\u2640"#♀
+                u"\u2019"
+                u"\u2708"#✈️
+                u"\u2695"#⚕
+                                   "]+", flags=re.UNICODE)
     list_items = []
     for item in answers.split(','):
         list_items.append(item)
     # Хорошенько перемешаем все элементы
     #random.shuffle(list_items)
     # Заполняем разметку перемешанными элементами
+
     for item in list_items:
-        but = types.InlineKeyboardButton(text = item,callback_data = 'new')
-        print('2')
+        but = types.InlineKeyboardButton(text = item,callback_data = callback + ","+emoji_pattern.sub(r'', item))
+        print(callback + ","+emoji_pattern.sub(r'', item))
         markup.add(but)
         print('3')
 
