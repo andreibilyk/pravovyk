b23f1706ac3b8619cd7d48327f8471b1ed624a4e
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-13 17:42:44 +0300

    init

--- bot.py
+++ bot.py
@@ -169,7 +169,6 @@ def contact_sent(message):
 @bot.callback_query_handler(func=lambda call: True) #-----InlineKeyboardButton
 def callback_inline(call):
     if call.message:
-     print(call)
      print(call.data)
      #print(list_items[-1])
      if call.data in network:
