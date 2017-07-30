f03930e3fc70b57fa2c7726b27c647a93ff0eda8
andreibilyk <30546658+andreibilyk@users.noreply.github.com>
2017-07-30 12:33:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -202,7 +202,7 @@ server = Flask(__name__)
 def verify():
  return "Hello world!"
 
-@server.route("/bot", methods=['POST'])
+@server.route("/", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
